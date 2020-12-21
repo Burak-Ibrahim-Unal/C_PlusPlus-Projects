0a1e110d19e689e96b313bc2f6cab754e94a86c4
burakibrahim@gmail.com <burakibrahim@gmail.com>
2020-12-21 14:06:52 +0300

    5th Class example fixed

--- Example Codes/Class_Example5.cpp
+++ Example Codes/Class_Example5.cpp
@@ -2,52 +2,60 @@
 
 using namespace std;
 
-class Covid {
+class Covid
+{
 
-   public:
-      int getYear( void );
-      Covid( int year );             // simple constructor
-      Covid( const Covid &obj);      // copy constructor
-      ~Covid();                      // destructor
- 
-   private:
-      int *p;
+public:
+     int getYear(void);
+     Covid(int year);         // simple constructor
+     Covid(const Covid &obj); // copy constructor
+     ~Covid();                // destructor
+
+private:
+     int *p;
 };
 
 // Member functions definitions including constructor
-Covid::Covid(int year) {
-   cout << "Normal constructor allocating p" << endl;
-   
-   // allocate memory for the pointer;
-   p = new int;
-   *p = year;
+Covid::Covid(int year)
+{
+     cout << "Normal constructor allocating p" << endl;
+
+     // allocate memory for the pointer;
+     p = new int;
+     *p = year;
 }
 
-Covid::Covid(const Covid &obj) {
-   cout << "Copy constructor allocating p." << endl;
-   p = new int;
-   *p = *obj.p; // copy the value
+Covid::Covid(const Covid &obj)
+{
+     cout << "Copy constructor allocating p." << endl;
+     p = new int;
+     *p = *obj.p; // copy the value
 }
 
-Covid::~Covid(void) {
-   cout << "Freeing memory!" << endl;
-   delete p;
+Covid::~Covid(void)
+{
+     cout << "Freeing memory!" << endl;
+     delete p;
 }
 
-int Covid::getYear( void ) {
-   return *p;
+int Covid::getYear(void)
+{
+     return *p;
 }
 
-void display(Covid obj) {
-   cout << "year of Covid : " << obj.getYear() <<endl;
+void display(Covid obj)
+{
+     cout << "year of Covid : " << obj.getYear() << endl;
 }
 
 // Main function for the program
-int main() {
-   Covid Covid(10);
+int main()
+{
+     Covid covid(2020);
+     Covid covid2 = covid;
 
-   display(Covid);
+     display(covid);
+     display(covid2);
 
-   return 0;
+     return 0;
 }
-
