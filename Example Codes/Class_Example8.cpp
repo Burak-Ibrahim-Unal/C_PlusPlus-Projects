03a4866fc5e86a793aafd5e85394360059719bef
burakibrahim@gmail.com <burakibrahim@gmail.com>
2020-12-21 21:10:11 +0300

    8th Class Example changed

--- Example Codes/Class_Example8.cpp
+++ Example Codes/Class_Example8.cpp
@@ -1,58 +1,64 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
- 
-class Shape {
-   protected:
-      int width, height;
-      
-   public:
-      Shape( int a = 0, int b = 0){
-         width = a;
-         height = b;
-      }
-      virtual int area() {
-         cout << "Parent class area :" <<endl;
-         return 0;
-      }
+
+class Shape
+{
+protected:
+    int width, height;
+
+public:
+    Shape(int a = 0, int b = 0)
+    {
+        width = a;
+        height = b;
+    }
+    virtual int area()
+    {
+        cout << "Parent class area :" << endl;
+        return 0;
+    }
 };
-class Rectangle: public Shape {
-   public:
-      Rectangle( int a = 0, int b = 0): Shape(a, b) { }
-      
-      int area () { 
-         cout << "Rectangle class area :" <<endl;
-         return (width * height); 
-      }
+class Rectangle : public Shape
+{
+public:
+    Rectangle(int a = 0, int b = 0) : Shape(a, b) {}
+
+    int area()
+    {
+        cout << "Rectangle class area :" << width * height << endl;
+        return (width * height);
+    }
 };
 
-class Triangle: public Shape {
-   public:
-       Triangle(int a = 0, int b = 0) : Shape(a, b){};
+class Triangle : public Shape
+{
+public:
+    Triangle(int a = 0, int b = 0) : Shape(a, b){};
 
-       int area()
-       {
-           cout << "Triangle class area :" << endl;
-           return (width * height / 2); 
-       }
+    int area()
+    {
+        cout << "Triangle class area :" << width * height / 2 << endl;
+        return (width * height / 2);
+    }
 };
 
 // Main function for the program
-int main() {
-   Shape *shape;
-   Rectangle rec(10,7);
-   Triangle  tri(10,5);
+int main()
+{
 
-   // store the address of Rectangle
-   shape = &rec;
-   
-   // call rectangle area.
-   shape->area();
+    Rectangle rec(10, 7);
+    Triangle tri(10, 5);
 
-   // store the address of Triangle
-   shape = &tri;
-   
-   // call triangle area.
-   shape->area();
-   
-   return 0;
+    // store the address of Rectangle
+    Shape *shape1 = &rec;
+    // call rectangle area.
+    shape1->area();
+
+    // store the address of Triangle
+    Shape *shape2 = &tri;
+
+    // call triangle area.
+    shape2->area();
+
+    return 0;
 }
